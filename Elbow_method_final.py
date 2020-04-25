bd7a64a1a2f7caade5871a02af123afdeb63b61e
elvinaeury <61745885+elvinaeury@users.noreply.github.com>
2020-04-26 00:11:09 +0200

    Add files via upload

--- Elbow_method_final.py
+++ Elbow_method_final.py
@@ -21,104 +21,196 @@ import seaborn as sns
 from scipy import spatial
 from time import time
 from sklearn.base import TransformerMixin
+from scipy.spatial.distance import cdist
+from kneed import KneeLocator
 
 
 # =============================================================================
 # Méthode du coude en utilisant deux méthodes
 # =============================================================================
+# Remove any NA
+class DataFrameImputer(TransformerMixin):
 
+    def __init__(self):
+        """Impute missing values.
 
+        Columns of dtype object are imputed with the most frequent value 
+        in column.
 
-class Elbow:
+        Columns of other types are imputed with mean of column.
+        """
+        
+    def fit(self, X, y=None):
 
-    def kmeans_manual(k,X,e):          
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].median() for c in X],
+            index=X.columns)
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
+
     
-        """
-        k: clusters
-        X: datapoints, in the form of a dataframe
-        e: by default 10**(-10)
-        initial_centers: determined by one of the initialisation algorithms
-        """
+def distance_squared(x,y):
+    return spatial.distance.cdist(x,y,'sqeuclidean')
+
+
+def random_init(k,X):
+    
+    sample,features=X.shape
+    centers=np.empty((k,features))
+
+    for i in range(k):
+        # tirage sans remise
+        centers[i]=X.sample(n=1,replace=False)
+        
+    return centers
+
+def kpp_init_notrials(k,X):
+        """On crée le premier centre, obtenu aléatoirement de nos observations """
+        
         sample,features=X.shape
-# =============================================================================
-# Initialisation random
-# =============================================================================
+    
         centers=np.empty((k,features))
-       
-        rands_deja_obtenus = set()
-        for i in range(k):
-            n = len(rands_deja_obtenus)
-            while len(rands_deja_obtenus) == n :
-                rand=npr.randint(0,sample)
-                rands_deja_obtenus.add(rand)
-            centers[i]=X.iloc[rand]
-# =============================================================================
-# Lloyd
-# =============================================================================           
-        error=e+1 
-        sse=0
-        sum_distance=0
-        # Assignation des centres
-        while error>e:
-            distances=np.empty((sample,k))
-            #sse=np.empty((sample,k))
-            clusters=np.empty(features)
-    
-            for i in range(k):
-                distances[:,i]=spatial.distance.cdist(np.atleast_2d(centers[i]),X,'sqeuclidean')
+        rand=npr.randint(0,sample)
+        # le premier centre
+        centers[0]=X.iloc[rand,:]
+        
+        """On obtient les distances de ce premier centre"""
+        dist=distance_squared(np.atleast_2d(centers[0]), X)
+        #Verification de la premiere distance:  print(np.sum(centres[0]-X.iloc[0,:])**2))
+        dist_total=np.sum(dist)
+        dist_cumul=np.cumsum(dist)
+        
+        """ On choisi les prochains centres (k-1 restants) en utilisant la probabilité proportionelle à la distance euclidienne au carré"""
+        for c in range(1,k):
+            random_proba=npr.random() # pas d'essaie dans ce cas, nous n'avons qu'une seule probabilité aléatoire
+            random_value=random_proba*dist_total
+            # On trouve l'indice de l'observation correspondant au random_value
+            candidate_id=np.searchsorted(dist_cumul,random_value)
+            
+            # Calculer la distance entre le candidat et les observations
+            dist_candidate=distance_squared(np.atleast_2d(X.iloc[candidate_id,:]),X)
+            
+            # On calcule la distance minimale entre la distance du candidat et la distance du premier centre
+            new_dist=np.minimum(dist_candidate,dist)
+            new_total_dist=np.sum(new_dist)
+            
+            
+            # On stock les centres et les distances minimum
+            centers[c]=X.iloc[candidate_id]
+#            print(X.iloc[candidate_id])
+            
+            dist_total=new_total_dist
+            dist=new_dist
+            dist_cumul = np.cumsum(dist)
+        return centers
 
-            clusters=np.argmin(distances,axis=1)
+
+
+# =============================================================================
+# Algorithme de lloyd
+# =============================================================================
+
+def lloyd(k,X,e,centers):          
     
-            # Mise a jour des centres
-            new_centers=deepcopy(centers)
-            for i in range(k):
-                new_centers[i,:]=np.mean(X.iloc[clusters==i],axis=0)
-                sse=spatial.distance.cdist(np.atleast_2d(new_centers),X.iloc[clusters==i],'sqeuclidean')
-                print(sse)
-                sum_distance=np.sum(sse,axis=1)
-                
-            
-            error=np.linalg.norm(centers-new_centers)
-            
-            print(sum_distance)
-            
-            centers=deepcopy(new_centers)
+    """
+    k: clusters
+    X: datapoints, in the form of a dataframe
+    e: by default 10**(-10)
+    initial_centers: determined by one of the initialisation algorithms
+    """
+    sample,features=X.shape
     
-        return [centers,clusters,sum_distance]
+#    print(centers)
+    error=e+1 # On initialise l'erreur: nombre aléatoire > 0
+
+    # Assignation des centres
+    while error>e:
+        distances=np.empty((sample,k))
+        clusters=np.empty(features)
+
+        for i in range(k):
+            distances[:,i]=distance_squared(np.atleast_2d(centers[i]),X)
+
+        # Les clusters sont formés à partir des candidats ayant les distances minimales
+        clusters=np.argmin(distances,axis=1)
+
+        # Mise a jour des centres
+        new_centers=deepcopy(centers)
+        for i in range(k):
+            new_centers[i,:]=np.mean(X.iloc[clusters==i],axis=0)
+            print(X.iloc[clusters==i])
+        error=np.linalg.norm(centers-new_centers)
+        
+        centers=deepcopy(new_centers)
+        
+    return [centers,clusters]
+
+
+def elbow_manual(n_clusters,X):
+    sample,features=X.shape
+    e=10**(-10)
+    # ici on force l'initialisation aléatoire
+    initial_centers=kpp_init_notrials(n_clusters,X)
+#    initial_random=random_init(n_clusters,X)
+    X = DataFrameImputer().fit_transform(X)
+#    X.fillna(X.mean())
+    
+    SSE=[]
+    SSE1=[]
+    for i in range(1,n_clusters):
+        centers,labels=lloyd(i,X,e,initial_centers)
+        centers_sk,labels_sk=kmean_sklearn(i,X)
+    
+        SSE.append(np.sum(np.min(cdist(X,centers,'euclidean'),axis=1)))
+        SSE1.append(np.sum(np.min(cdist(X,centers_sk,'euclidean'),axis=1)))
+    
+    K=np.arange(1,n_clusters)    
+    plt.plot(K,SSE)
+    plt.title('Méthode du coude')
+    plt.show()
+    
+#    # On doit prendre au minimum 2 clusters
+#    K_=np.arange(2,n_clusters)
+#    kn = KneeLocator(K_, SSE, curve='convex', direction='decreasing')    
+#    print(kn.knee)
+#    # plotting dashed_vline on knee
+#    plt.vlines(kn.knee, plt.ylim()[0], plt.ylim()[1], linestyles='dashed')
+
+# =============================================================================
+# Sklearn elbow method
+# =============================================================================
+
+def kmean_sklearn(k,X):
+    k_means = KMeans(n_clusters=k)
+    k_means.fit(X)
+    
+    centers = k_means.cluster_centers_
+    labels=k_means.fit_predict(X)
     
     
-    def kmean_sklearn(k,X):
-        k_means = KMeans(n_clusters=k)
+    return [centers,labels]
+    
+    
+    
+def elbow_sklearn(n_clusters,X):
+    SSE=[]
+    k=np.arange(1,n_clusters)
+    for i in range(1,n_clusters):
+        k_means = KMeans(i)
         k_means.fit(X)
+        kmeans_inertia=k_means.inertia_
         
-        centers = k_means.cluster_centers_
-        labels=k_means.fit_predict(X)
-        inertia=k_means.inertia_
-        print(inertia)
-        
-        return [centers,labels,inertia]
-        
-        
-        
-    def elbow_manual(X,e):
-        k = np.arange(1,5)
-        for i in range(1,5):
-            kmeans_inertia=Elbow.kmeans_manual(i,X,e)[2]
-        plt.plot(k, kmeans_inertia, 'bx-')
-        plt.xlabel('k')
-        plt.ylabel('Somme_distance_carre')
-        plt.title('Méthode du coude')
-        plt.show()
-        
-        
-    def elbow_sklearn(X):
-        SSE=[]
-        k=np.arange(1,5)
-        for i in range(1,5):
-            kmeans_inertia=Elbow.kmean_sklearn(i,X)[2]
-            SSE.append(kmeans_inertia)
-        plt.plot(k, SSE, 'bx-')
-        plt.show()
+        SSE.append(kmeans_inertia)
+    plt.plot(k, SSE, 'bx-')
+    plt.show()
+    kn = KneeLocator(k, SSE, curve='convex', direction='decreasing')    
+    print(kn.knee)
+    # plotting dashed_vline on knee
+    plt.vlines(kn.knee, plt.ylim()[0], plt.ylim()[1], linestyles='dashed')
+
+
 
 
 # =============================================================================
@@ -127,17 +219,24 @@ class Elbow:
 
 if __name__=="__main__":
     
-    iris=datasets.load_iris()
-    data = pd.DataFrame(data= np.c_[iris['data'], iris['target']],columns= iris['feature_names'] + ['target'])
-
-    X=data.iloc[:,0:4]
-#    Y = data['target']
+    base=pd.read_csv('/Users/elvinagovendasamy/TER/BigML_Dataset.csv',sep=',')
     
-    k,e=3,10**(-10)
+    X=base.iloc[:,1:]
+    Y=base.iloc[:,0]
+    
+    # Imputing based on mean for numeric, and most frequent for strings
+    X = DataFrameImputer().fit_transform(X)
+    X.fillna(X.mean())
+#    Y.fillna(Y.mean())
+    
+    k,e=5,10**(-10)
+    
+    n_clusters=7
+    
+#    kpp=kpp_init_notrials(k,X)
+#    random_=random_init(k,X)
+#    lloyd_=lloyd(k,X,e,kpp)
     
     
-    
-
-    
-#    Elbow.elbow_manual(X,e)
-    Elbow.elbow_sklearn(X)
+    graph1=elbow_manual(n_clusters,X)
+#    elbow_sklearn(n_clusters,X)
